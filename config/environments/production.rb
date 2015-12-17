4e1e9099529bb325ee060127952a183ab313a298
mlavin <martylavin1@yahoo.com>
2015-12-16 23:26:14 -0600

    went back to gmail to send email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,18 +54,13 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
 
-
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-  :address              => "smtp.sendgrid.net",
+  :address              => "smtp.gmail.com",
   :port                 => 587,
-  :user_name            => ENV["SENDGRID_USERNAME"],
-  :password             => ENV["SENDGRID_PASSWORD"],
-  #:authentication       => 'plain',
-  :authentication => :plain,
-  :domain         => 'heroku.com',
+  :user_name            => ENV["GMAIL_USERNAME"],
+  :password             => ENV["GMAIL_PASSWORD"],
+  :authentication       => 'plain',
   :enable_starttls_auto => true  }
-
-
 
 end
