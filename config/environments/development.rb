8d8b23d213151c2776464692b846f5f2ccdf2dc2
mlavin <martylavin1@yahoo.com>
2016-01-19 18:19:37 -0600

    changed from sendgrid back to googlemail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,19 +34,6 @@ Rails.application.configure do
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
-  #config.assets.raise_runtime_errors = true
-
-  #config.action_mailer.perform_deliveries = true
-
-
-  #config.action_mailer.delivery_method = :smtp
-  #config.action_mailer.smtp_settings = {
-  #:address              => "smtp.gmail.com",
-  #:port                 => 587,
-  #:user_name            => ENV["GMAIL_USERNAME"],
-  #:password             => ENV["GMAIL_PASSWORD"],
-  #:authentication       => 'plain',
-  #:enable_starttls_auto => true  }
 
   config.assets.raise_runtime_errors = true
 
@@ -55,11 +42,25 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-  :address              => "smtp.sendgrid.net",
+  :address              => "smtp.gmail.com",
   :port                 => 587,
-  :user_name            => ENV["SENDGRID_USERNAME"],
-  :password             => ENV["SENDGRID_PASSWORD"],
+  :user_name            => ENV["GMAIL_USERNAME"],
+  :password             => ENV["GMAIL_PASSWORD"],
   :authentication       => 'plain',
   :enable_starttls_auto => true  }
+
+  #config.assets.raise_runtime_errors = true
+
+  #config.action_mailer.perform_deliveries = true
+
+
+  #config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.smtp_settings = {
+  #:address              => "smtp.sendgrid.net",
+  #:port                 => 587,
+  #:user_name            => ENV["SENDGRID_USERNAME"],
+  #:password             => ENV["SENDGRID_PASSWORD"],
+  #:authentication       => 'plain',
+  #:enable_starttls_auto => true  }
 
 end
