7b919a6d80ece61c4d6786b02df88dca0640c013
mlavin <martylavin1@yahoo.com>
2016-01-19 18:31:27 -0600

    changed productin from sendgrid to googlemail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,20 +50,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-   #config.assets.raise_runtime_errors = true
-
-  #config.action_mailer.perform_deliveries = true
-
-  #config.action_mailer.delivery_method = :smtp
-  #config.action_mailer.smtp_settings = {
-  #:address              => "smtp.gmail.com",
-  #:port                 => 587,
-  #:user_name            => ENV["GMAIL_USERNAME"],
-  #:password             => ENV["GMAIL_PASSWORD"],
-  #:authentication       => 'plain',
-  #:enable_starttls_auto => true  }
-
-
    config.assets.raise_runtime_errors = true
 
   config.action_mailer.perform_deliveries = true
@@ -71,13 +57,28 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-  :address              => "smtp.sendgrid.net",
+  :address              => "smtp.gmail.com",
   :port                 => 587,
-  :user_name            => ENV["SENDGRID_USERNAME"],
-  :password             => ENV["SENDGRID_PASSWORD"],
-  #:user_name            => 'martylavin1',
-  #:password             => 'Vickieelder1',
+  :user_name            => ENV["GMAIL_USERNAME"],
+  :password             => ENV["GMAIL_PASSWORD"],
   :authentication       => 'plain',
   :enable_starttls_auto => true  }
+
+
+  # config.assets.raise_runtime_errors = true
+
+  #config.action_mailer.perform_deliveries = true
+
+
+  #config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.smtp_settings = {
+  #:address              => "smtp.sendgrid.net",
+  #:port                 => 587,
+  #:user_name            => ENV["SENDGRID_USERNAME"],
+  #:password             => ENV["SENDGRID_PASSWORD"],
+  ##:user_name            => 'martylavin1',
+  ##:password             => 'Vickieelder1',
+  #:authentication       => 'plain',
+  #:enable_starttls_auto => true  }
 
 end
