670e007ff22a9240633e8b826ce301e4c6e91bfa
mlavin <martylavin1@yahoo.com>
2016-03-16 21:05:18 -0500

    changed to pure gmail pass adn user email in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,14 +58,18 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :user_name            => ENV["GMAIL_USERNAME"],
-  :password             => ENV["GMAIL_PASSWORD"],
+  #:user_name            => ENV["GMAIL_USERNAME"],
+  #:password             => ENV["GMAIL_PASSWORD"],
+  :user_name            => 'martylavin1@gmail.com',
+  :password             => 'Vickieelder1',
   :authentication       => 'plain',
   :enable_starttls_auto => true  }
 
 
   # config.assets.raise_runtime_errors = true
+
   #config.action_mailer.perform_deliveries = true
+
 
   #config.action_mailer.delivery_method = :smtp
   #config.action_mailer.smtp_settings = {
@@ -73,6 +77,8 @@ Rails.application.configure do
   #:port                 => 587,
   #:user_name            => ENV["SENDGRID_USERNAME"],
   #:password             => ENV["SENDGRID_PASSWORD"],
+  #:user_name            => 'martylavin1',
+  #:password             => 'Vickieelder1',
   #:authentication       => 'plain',
   #:enable_starttls_auto => true  }
 
