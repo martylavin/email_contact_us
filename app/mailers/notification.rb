d4d6642a2e91b36007853affda26fa9cb72d79ef
mlavin <martylavin1@yahoo.com>
2016-03-23 22:36:20 -0500

    c hanged sendgrid pass

--- app/mailers/notification.rb
+++ app/mailers/notification.rb
@@ -2,6 +2,6 @@ class Notification < ApplicationMailer
   def new_account(user)
     @user = user
     #attachments['rails.2016_JanFebNL.pdf'] = File.read("app/assets/images/2016_JanFebNL.pdf")
-    mail(to: "alpost854il@aol.com", subject: "The message from #{user.name} has beens sent to Evergreen Park Legion Post 854")
+    mail(to: "alpost854il@aol.com", subject: "The message from #{user.name} has been sent to Evergreen Park Legion Post 854")
   end
 end
